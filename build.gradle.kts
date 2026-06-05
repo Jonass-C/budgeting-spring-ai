48cfe892d1bc2a24eb1eee670f344e541b7d8e95
Jonass-C <brjonas13@gmail.com>
2026-06-05 12:37:42 -0300

    feat(ai-integration): configura Spring AI e conexao com Gemini
    - adiciona dependencias starters do Spring e GenAI
    - implementa endpoint para conexao do REST com o prompt
    - cria teste de integracao para validacao da API com o GenAI

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+	java
+	id("org.springframework.boot") version "4.0.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "dio.jonas"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springAiVersion"] = "2.0.0-M8"
+
+dependencies {
+	implementation("org.springframework.ai:spring-ai-starter-model-google-genai")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
